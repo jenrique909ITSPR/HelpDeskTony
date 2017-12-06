fa6a7eba11f2cdccf9bcf9447217fa3ea527411e
elenavaldes <32577023+elenavaldes@users.noreply.github.com>
2017-12-06 12:12:06 -0600

    funcion de correo
    se agrego la funcion de correo

--- config/app.php
+++ config/app.php
@@ -226,7 +226,7 @@ return [
             'persistent' => false,
 
 
-            'host' => 'localhost',
+            'host' => '10.105.160.50',
 			     // 'host' => 'localhost',
 
             /**
@@ -235,8 +235,8 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'root',
-		        'password' => '',
+            'username' => 'cakephp',
+		        'password' => 'cakephp',
             'database' => 'activos_tickets',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
